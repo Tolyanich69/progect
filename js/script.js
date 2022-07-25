a4082f73bc2e278e5403afdc26ebd64b34ba379a
Igor <progr2000@gmail.com>
2022-07-25 23:27:57 +0300

    Lesson 22

--- js/script.js
+++ js/script.js
@@ -9,18 +9,18 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один з останніх переглянутих фільмів?', ''),
+          b = prompt('На скільки оціните його?', '');
+          if (a !=null && b != null && a != '' && b != '' & a.length < 50) {
+              personalMovieDB.movies[a] = b;
+              console.log('done');
+          } else {
+              console.log('error');
+              i--;
+          }
+    
+}
 console.log(personalMovieDB);
 
-for (let i = 0; i < 3; i++) {
-    console.log(i);
-    for (let j = 0; j < 3; j++) {
-        console.log(j);
-    }
-}
