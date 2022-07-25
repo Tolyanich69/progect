c604e377f8f3bd9bb2be66215a72c7d2a7066946
Igor <progr2000@gmail.com>
2022-07-25 22:10:19 +0300

    test

--- js/script.js
+++ js/script.js
@@ -17,3 +17,10 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+}
