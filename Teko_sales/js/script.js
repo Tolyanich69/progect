2295f8e10a9192a6cb557c0a1bb8d070f3a807e7
Igor <progr2000@gmail.com>
2022-08-28 22:39:34 +0300

    Project_Teko_sales

---
+++ Teko_sales/js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const urlRequest = 'https://script.google.com/macros/s/AKfycbxRr\
+-fKBYaGxkNV88BssO0x7mHgrQaxQl-VhdqJWy4P-QbMptxChuAwWtJQKVsCmbfa/exec';
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', urlRequest);
+xhr.responseType ='json';
+xhr.send();
+
+xhr.onload = () => {
+    console.log(xhr.response);
+};
+
+function viewInTable(data) {
+    
+}
