40d84e5983038bc5298bb2a4b57fb5744f0ab817
Igor <progr2000@gmail.com>
2022-08-29 16:35:01 +0300

    Tekosales_2

--- Teko_sales/js/script.js
+++ Teko_sales/js/script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const urlRequest = 'https://script.google.com/macros/s/AKfycbxRr\
--fKBYaGxkNV88BssO0x7mHgrQaxQl-VhdqJWy4P-QbMptxChuAwWtJQKVsCmbfa/exec';
+const urlRequest = 'https://script.google.com/macros/s/AKfycbxRr-fKBYaGxkNV88BssO0x7mHgrQaxQl-VhdqJWy4P-QbMptxChuAwWtJQKVsCmbfa/exec';
 
 const xhr = new XMLHttpRequest();
 xhr.open('GET', urlRequest);
